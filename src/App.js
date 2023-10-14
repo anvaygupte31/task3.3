0d40008fc5e967bfc5ce4088a665ad3defeb407f
Your Name <you@example.com>
2023-10-14 23:08:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ColorPicker from "./color-picker.js"
+import "./color-picker.css"
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    
+    const colors = ['#FF0000', '#0000FF', '#008000', '#FFFF00', '#800080', '#FFFFFF', '#c71585', '#00FF00', '#a52a2a', '#ffd700', '#ffe4b5', '#ffa500', '#808080', '#ffb6c1', '#6a5acd']
+    
+    return(
+        <div>
+            <h1>Color Picker</h1>
+            <div>
+                <div className="color-switch">
+                    <ColorPicker colors={ colors } />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
